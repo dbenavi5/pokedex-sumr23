25b0b4141aba20559c4cdc3ed3e35aa46b8dc2e7
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-06-12 20:42:26 -0700

    initial commit- add assets and setup site layout

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,15 @@
+import {MdOutlinePowerSettingsNew} from 'react-icons/md'
+
+function Footer() {
+  return (
+    <footer className=''>
+      <div className="block"></div>
+      <div className="data"></div>
+      <div className="block">
+        <MdOutlinePowerSettingsNew/>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import pokeballIcon from '../assets/pokeball-icon.png';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt='pokeball icon' className=''/>
+      </div>
+      <div className="data"></div>
+      <div className="block">
+        <GiHamburgerMenu/>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
