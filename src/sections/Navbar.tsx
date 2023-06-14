10e5ebfbf81235ae052e449f9871eb292a58bd2b
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-06-13 19:46:34 -0700

    add navbar links&routes and style link selection

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,19 +1,76 @@
-import React from 'react'
-import pokeballIcon from '../assets/pokeball-icon.png';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import React, { useEffect } from "react";
+import pokeballIcon from "../assets/pokeball-icon.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link, useLocation } from "react-router-dom";
+// import { useAppDispatch } from "../app/hooks";
 
 function Navbar() {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const navigationRoutes = [
+    {
+      name: "Search",
+      route: "/search",
+    },
+    {
+      name: "Compare",
+      route: "/compare",
+    },
+    {
+      name: "Pokemon",
+      route: "/pokemon",
+    },
+    {
+      name: "My List",
+      route: "/list",
+    },
+    {
+      name: "About",
+      route: "/about",
+    },
+  ];
+
+  const location = useLocation();
+  // const dispatch = useAppDispatch();
+
+  // create navbar link underline selection effect
+
+  useEffect(() => {
+    const index = navigationRoutes.findIndex(({ route }) =>
+      location.pathname.includes(route)
+    );
+
+    ul(index);
+  }, [location.pathname, navigationRoutes]);
+  function ul(index: number) {
+    const underlines = document.querySelectorAll<HTMLElement>(".underline");
+    for (let i = 0; i < underlines.length; i++) {
+      underlines[i].style.transform = "translate3d(" + index * 100 + "%,0,0)";
+    }
+  }
   return (
     <nav>
       <div className="block">
-        <img src={pokeballIcon} alt='pokeball icon' className=''/>
+        <img src={pokeballIcon} alt="pokeball icon" className="" />
       </div>
-      <div className="data"></div>
+      <div className="data">
+        <ul>
+          <div className="underline"></div>
+          <div className="underline"></div>
+          <div className="underline"></div>
+          {navigationRoutes.map(({ name, route }, index) => {
+            return (
+              <Link to={route} key={index}>
+                <li>{name}</li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
       <div className="block">
-        <GiHamburgerMenu/>
+        <GiHamburgerMenu />
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/sections/Wrapper.tsx
+++ src/sections/Wrapper.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function Wrapper() {
+const Wrapper = (Component: React.FC) => () => {
   return (
-    <div className='content'>Wrapper</div>
-  )
-}
+    <div className="content">
+      <Component />
+    </div>
+  );
+};
 
-export default Wrapper
+export default Wrapper;
